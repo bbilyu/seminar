53277f3c495ad2bd60d2c84640b4769571e7eb63
Илья Белоглазов <bbilyu@mail.ru>
2022-08-05 13:31:01 +0500

    Task2

---
+++ Task1/Program.cs
@@ -0,0 +1,13 @@
+﻿
+Console.Write("Введите первое число: ");
+int num_1 = int.Parse(Console.ReadLine()); 
+Console.Write("Введите второе число: ");
+int num_2 = int.Parse(Console.ReadLine());
+if (num_1 == num_2*num_2 )
+{
+Console.Write($"Число {num_1} является квадратом числа {num_2} ");
+}
+else
+{
+Console.Write($"Число {num_1} не является квадратом числа {num_2} ");
+}
