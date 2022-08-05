21a80e2ffa40e3b60b580b468e77355e3756ffdf
Илья Белоглазов <bbilyu@mail.ru>
2022-08-05 14:43:56 +0500

    Add Task7

---
+++ Task7/Program.cs
@@ -0,0 +1,10 @@
+﻿
+Console.Write("Введите трёхзначное число: ");
+int num = Convert.ToInt32(Console.ReadLine()); 
+if (num > 99 && num < 1000)
+{
+    Console.Write($"Последняя цифра числа {num} = {num % 10}");
+}
+else 
+Console.Write("Число не трёхзеначное");
+
