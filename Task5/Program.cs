a80db9d8e031bcca7533fd37fc483586f65d652a
Илья Белоглазов <bbilyu@mail.ru>
2022-08-05 14:18:43 +0500

    add Task5

---
+++ Task5/Program.cs
@@ -0,0 +1,11 @@
+﻿
+Console.Write("Введите число N: ");
+int n = int.Parse(Console.ReadLine()); 
+int x = n * (-1);
+Console.Write($"{n}->'");
+while (x <= n)
+{
+    Console.Write($"{x} ");
+    x++;
+}
+Console.Write("'");
