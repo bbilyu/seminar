5fc63daad0982250a6672b5e43170d86863bf810
Илья Белоглазов <bbilyu@mail.ru>
2022-08-19 14:31:44 +0500

    seminar_5

---
+++ Seminar5_Task32/Program.cs
@@ -0,0 +1,41 @@
+﻿
+
+
+Console.Clear();
+
+int[] CreateArrayRndInt(int size, int min, int max)
+{
+    int[] array = new int[size];
+    Random rnd = new Random();
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = rnd.Next(min, max + 1);
+    }
+    return array;
+}
+
+int[] ReplacePosElemArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+       if (array[i] > 0) array[i] = - array[i];
+    }
+
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i < array.Length -1) Console.Write($"{array[i]},");
+        else Console.Write(array[i]);
+    }
+    Console.WriteLine("]");
+}
+
+int[] arr = CreateArrayRndInt(12, -9, 9);
+
+PrintArray(arr);
+PrintArray(ReplacePosElemArray(arr));
---
+++ Seminar5_Task37/Program.cs
@@ -0,0 +1,52 @@
+﻿// Задача 37: Найдите произведение пар чисел в одномерном массиве.
+// Парой считаем первый и последний элемент, второй и предпоследний
+// и т.д. Результат запишите в новом массиве.
+// [1 2 3 4 5] -> 5 8 3
+// [67  3 6] -> 36 21
+
+Console.Clear();
+
+int[] CreateArrayRndInt(int min, int max)
+{
+    Random rnd = new Random();
+    int[] array = new int[rnd.Next(4,7)];
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = rnd.Next(min, max + 1);
+    }
+
+    return array;
+}
+
+int[] NewArray(int[] array)
+{
+    int size = array.Length / 2;
+    if (array.Length % 2 != 0) size++;
+    int[] result = new int[size];
+    for (int i = 0; i < array.Length/2; i++)
+    {
+        result[i] = array[i] * array[array.Length - i - 1];
+    }
+    if (array.Length % 2 != 0) result[size - 1] = array[array.Length/2];
+    
+    return result;
+} 
+
+void PrintArray(int[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i < array.Length -1) Console.Write($"{array[i]},");
+        else Console.Write(array[i]);
+    }
+    Console.Write("]");
+}
+
+int[] arr = CreateArrayRndInt(1, 9);
+
+int[] newarray = NewArray(arr);
+
+PrintArray(arr);
+Console.Write("->");
+PrintArray(newarray);
