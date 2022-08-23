e190fba79d0b93e8c73fa67bf92d22553db1f8cd
Илья Белоглазов <bbilyu@mail.ru>
2022-08-23 14:44:08 +0500

    seminar_6

---
+++ Seminar6_Task42/Program.cs
@@ -0,0 +1,29 @@
+﻿// Задача 42: Напишите программу, которая будет преобразовывать
+// десятичное число в двоичное.
+// 45 -> 101101
+// 3 -> 11
+// 2 -> 10
+
+Console.Clear();
+System.Console.Write("Введите десятичное чиcло -> ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int TransToBinary(int num)
+{
+    int result = 0;
+    int i = 1;
+
+    while (num > 0)
+    {
+        result += num % 2 * i;
+        num /= 2;
+        i *= 10;
+    }
+
+    return result;
+
+}
+
+int transToBinary = TransToBinary(number);
+
+Console.WriteLine($"Бинарное -> {transToBinary}");
